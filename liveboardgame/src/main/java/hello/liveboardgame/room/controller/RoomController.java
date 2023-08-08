29dafb2154b6aa795366aeb357f2b40e6ac10b92
HanSeoJin <yp6484@gmail.com>
2023-08-08 20:14:27 +0900

    [추가] RoomController구현

---
+++ liveboardgame/src/main/java/hello/liveboardgame/room/controller/RoomController.java
@@ -0,0 +1,24 @@
+package hello.liveboardgame.room.controller;
+
+import hello.liveboardgame.room.dto.RoomIdDto;
+import hello.liveboardgame.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class RoomController {
+    private final RoomService roomService;
+
+
+    @GetMapping("/roomId")
+    public RoomIdDto getRoomIdController(String xxxID) {
+        log.info("Get:/roomId 요청");
+        log.info("xxxID={}", xxxID);
+        return new RoomIdDto(roomService.getRoomId());
+    }
+}
---
+++ liveboardgame/src/main/java/hello/liveboardgame/room/dto/RoomIdDto.java
@@ -0,0 +1,10 @@
+package hello.liveboardgame.room.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RoomIdDto {
+    private Long roomId;
+}
