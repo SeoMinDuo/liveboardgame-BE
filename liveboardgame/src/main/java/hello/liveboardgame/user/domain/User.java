5317dd0d7322b776b90db1bb00d97ae19fd6336e
HanSeoJin <yp6484@gmail.com>
2023-08-16 22:02:40 +0900

    [수정] User 불필요 import삭제

--- liveboardgame/src/main/java/hello/liveboardgame/user/domain/User.java
+++ liveboardgame/src/main/java/hello/liveboardgame/user/domain/User.java
@@ -1,6 +1,5 @@
-package hello.liveboardgame.room.domain;
+package hello.liveboardgame.user.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +7,12 @@ import lombok.Setter;
 public class User {
 
     private Long id;
+    private String sessionId;
     private String name;
     private Long roomId;
 
-    public User(String name, Long roomId) {
+    public User(String sessionId, String name, Long roomId) {
+        this.sessionId = sessionId;
         this.name = name;
         this.roomId = roomId;
     }
