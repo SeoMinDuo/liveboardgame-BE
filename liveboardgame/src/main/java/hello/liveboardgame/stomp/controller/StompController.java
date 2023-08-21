4bac87f02860af3f59dc684cc74467471599899d
HanSeoJin <yp6484@gmail.com>
2023-08-21 21:26:50 +0900

    [삭제] STOMP 테스트용 gameRoomResponseController 삭제

--- liveboardgame/src/main/java/hello/liveboardgame/stomp/controller/StompController.java
+++ liveboardgame/src/main/java/hello/liveboardgame/stomp/controller/StompController.java
@@ -8,31 +8,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-
-import java.util.Map;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class StompController {
-    private final SimpMessageSendingOperations messagingTemplate;
     private final RoomService roomService;
-
-    @MessageMapping("/sendMessage")
-//    @SendTo("/topic/messages")
-    public void gameRoomResponseController(/*RoomMessage*/@Payload Map<String, String> message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-//        log.info("id={}", message.getId());
-//        log.info("msg={}", message.getMsg());
-//        log.info("roomId={}", message.getRoomId());
-        log.info("message.text = {}", message.get("text"));
-        Greeting greeting = new Greeting("일단 text받긴함");
-        //연결된 소켓의 세션정보를 얻음
-        log.info("sessionID={}", headerAccessor.getSessionId());
-//        messagingTemplate.convertAndSend("/topic/" + message.getRoomId(), message);
-        messagingTemplate.convertAndSend("/topic/" + /*message.getRoomId()*/"1", greeting);
-    }
 
     @MessageMapping("/enterRoom/{roomId}")
     @SendTo("/topic/{roomId}")
